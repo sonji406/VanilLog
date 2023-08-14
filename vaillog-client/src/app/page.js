7175d371862c0b2b06131d53fdcb43ce5f0dedea
sonji406 <wkaqh24@gmail.com>
2023-08-14 15:09:24 +0900

    Set: Next.JS 초기 세팅

---
+++ vaillog-client/src/app/page.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  )
+}
