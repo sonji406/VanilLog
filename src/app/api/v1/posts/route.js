0852792a784998b66f5f3e98bdc990fe08ac1d84
Josh5519 <AcForCompany@gmail.com>
2023-08-17 02:34:27 +0900

    Feat: 페이지네이션 적용, api 설명 주석 작성

--- src/app/api/v1/posts/route.js
+++ src/app/api/v1/posts/route.js
@@ -1,38 +1,13 @@
 import dbConnect from '@lib/dbConnect';
 import Post from '@models/Post';
-import { ObjectId } from 'mongodb';
 import mongoose from 'mongoose';
 import { NextResponse } from 'next/server';
 
-//GET /api/v1/posts?userId=:userId&page={page_number}&limit={items_per_page}
-//테스트용 http://localhost:3000/api/v1/posts?userId=64dceeca5528cd869ba560db&page=10&limit=5
-
-/* 성공
-{
-  "status": "success",
-  "data": [{
-	  "_id": "60ffdd8a13e44b5d1ec312e9",
-	  "title": "My Blog Post",
-	  "category": "programming",
-	  "author": {
-	    "_id": "60ffdc3a33fca84a8e05bb92",
-	    "nickname": "john_doe"
-		 },
-	  "createdAt": "2023-08-01T12:00:00.000Z",
-	  "updatedAt": "2023-08-01T12:34:56.789Z"
-  },
-  {....},
-  ....]
-}
-*/
-
-/* 실패
-{
-  "status": 500,
-  "message": "포스트 목록을 불러올 수 없습니다."
-}
-*/
-
+/**
+ * 포스트 목록 조회 API
+ * @URL /api/v1/posts?userId=:userId&page={page_number}&limit={items_per_page}
+ * @param request
+ */
 export const GET = async (request) => {
   await dbConnect();
   const successResponse = {
@@ -42,7 +17,6 @@ export const GET = async (request) => {
     status: 500,
   };
 
-  // 파라미터들 가져오기
   try {
     const { searchParams } = new URL(request.url);
     const userId = searchParams.get('userId');
@@ -57,11 +31,12 @@ export const GET = async (request) => {
       throw { message: '조회에 필요한 파라미터가 부족합니다', status: 400 };
     }
 
-    // 유저별 포스트 목록 가져오기
-    const posts = await Post.find({ author: userId }).exec();
+    // 작성자별 포스트 목록 가져오기(페이지네이션 적용)
+    const posts = await Post.find({ author: userId }, null, {
+      skip: (page - 1) * limit,
+      limit,
+    }).exec();
     successResponse.data = posts;
-
-    // 2차 페이징 포함해서 조회해 오기
 
     return NextResponse.json(successResponse);
   } catch (error) {
