a20cc938a07812f17c1e6bfa708d9332a85ad9c8
sonji406 <wkaqh24@gmail.com>
2023-08-16 22:38:07 +0900

    Set: tailwind 설정

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.jsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'Ambit', 'sans-serif'],
+      },
+      colors: {
+        logo: '#0044ff',
+      },
+    },
+  },
+  plugins: [],
+};
