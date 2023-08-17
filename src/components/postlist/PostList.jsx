5cfb94fd996fd0a7e9766b34b4691e45024f7154
sonji406 <wkaqh24@gmail.com>
2023-08-17 15:19:53 +0900

    Feat: 포스트 목록 조회

--- src/components/postlist/PostList.jsx
+++ src/components/postlist/PostList.jsx
@@ -2,6 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function PostList({ userId, page, limit }) {
   const queryParams = userId
@@ -38,7 +40,54 @@ function PostList({ userId, page, limit }) {
     }
   }, [userId, page, limit]);
 
-  return;
+  return (
+    <div>
+      <div>
+        <Link href='/post:userid'>
+          <button>포스트 작성하기</button>
+        </Link>
+      </div>
+      <div className='flex flex-row gap-x-8 gap-y-4 place-items-left text-center'>
+        {posts.length > 0 &&
+          posts.map((post) => {
+            const imageContent = post.content.find(
+              (content) => content.type === 'image',
+            );
+            const textContent = post.content.find(
+              (content) => content.type === 'text',
+            );
+            const textValue = textContent ? textContent.value : '';
+
+            return (
+              <Link key={post._id} href={`/posts/${post.author}/${post._id}`}>
+                <div className='w-56 h-56 flex flex-col items-center justify-center bg-slate-300'>
+                  {imageContent ? (
+                    <div className='h-4/5'>
+                      <Image
+                        src={imageContent.value}
+                        alt={textValue}
+                        width={224}
+                        height={224}
+                      />
+                    </div>
+                  ) : (
+                    <div className='h-4/5 flex items-center justify-center'>
+                      <span className='text-lg font-bold'>No Image</span>
+                    </div>
+                  )}
+                  <div className='flex items-center justify-center h-1/5 w-full bg-slate-400'>
+                    <span className='inline-block truncate w-48'>
+                      {textValue}
+                    </span>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
+      </div>
+      {error && <div>{error}</div>}
+    </div>
+  );
 }
 
 export { PostList };
