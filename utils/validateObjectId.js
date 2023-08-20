ae7388d9236b9ec2d88f70a31d14aa3516e66a4f
sonji406 <wkaqh24@gmail.com>
2023-08-20 22:27:30 +0900

    Fix: error 대문자 수정, 불필요한 조건 및 변수 선언 삭제,

--- utils/validateObjectId.js
+++ utils/validateObjectId.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import createError from 'http-errors';
-import { errors } from '@utils/errors';
+import { ERRORS } from '@utils/errors';
 
 export function validateObjectId(userId) {
   if (!mongoose.Types.ObjectId.isValid(userId)) {
     throw createError(
-      errors.INVALID_USER_ID.STATUS_CODE,
-      errors.INVALID_USER_ID.MESSAGE,
+      ERRORS.INVALID_USER_ID.STATUS_CODE,
+      ERRORS.INVALID_USER_ID.MESSAGE,
     );
   }
 }
