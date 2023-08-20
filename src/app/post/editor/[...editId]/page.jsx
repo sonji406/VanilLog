c516831e7ae0d1c6cc9c12f8f68e8134fb5fd63f
sonji406 <wkaqh24@gmail.com>
2023-08-20 14:06:16 +0900

    Feat: 포스트 생성 및 수정 페이지

---
+++ src/app/post/editor/[...editId]/page.jsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import axios from 'axios';
+import Navbar from '@src/components/Navbar';
+
+const Editor = dynamic(() => import('@src/components/Editor'), {
+  ssr: false,
+});
+
+function PostEditPage({ params }) {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState({});
+  const [error, setError] = useState(null);
+
+  const [userId, postId] = params.editId;
+
+  const isEditing = Boolean(postId);
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const setSaveError = (error) => {
+    setError(error);
+  };
+
+  useEffect(() => {
+    if (isEditing) {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(`/api/v1/posts/${postId}`);
+
+          if (response.data.status === 'success') {
+            setTitle(response.data.data.title);
+            setContent(response.data.data.content);
+          }
+
+          if (response.data.status !== 'success') {
+            setError(response.data.message);
+          }
+        } catch (e) {
+          setError('포스트를 불러오는 중 문제가 발생하였습니다.');
+        }
+      };
+
+      fetchData();
+    }
+  }, [isEditing, postId]);
+
+  return (
+    <>
+      <div className='flex justify-center'>
+        <Navbar />
+        <div className='w-[800px] text-center mt-6'>
+          <div className='flex items-center justify-center border-2 border-black'>
+            <input
+              type='text'
+              placeholder='제목을 입력하세요'
+              className='w-4/5 p-2 text-lg'
+              value={title}
+              onChange={handleTitleChange}
+            />
+          </div>
+          <Editor
+            author={userId}
+            postId={postId}
+            title={title}
+            content={content}
+            isEditing={isEditing}
+            saveError={error}
+            setSaveError={setSaveError}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PostEditPage;
