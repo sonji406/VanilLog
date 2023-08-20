9f5fbbadaae30d5aad08068e8b50724d65900001
Josh5519 <AcForCompany@gmail.com>
2023-08-21 01:27:34 +0900

    Feat: 화면 상단 컴포넌트 작성

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,50 @@
-//nextjs Head https://jasonkang14.github.io/nextjs/head-from-next-head-and-next-document
-//head.js special file https://jha-memo.tistory.com/97
+'use client';
 
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { LogoutButton } from './LogoutButton';
 
 function Navbar() {
-  const userId = '';
+  const { data, status } = useSession();
+  const userId = data?.mongoId;
+
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <>
+    <div className='flex justify-between items-center'>
       <Link href='/'>
         <span className='text-5xl text-logo font-bold'>vanilLog</span>
       </Link>
-      <Link href='/auth/login'>로그인</Link>
-      <Link href={`/posts/${userId}`}>내 블로그</Link>
-    </>
+      {status === 'unauthenticated' ? (
+        <>
+          <Link href='/auth/login' className='ml-4'>
+            로그인
+          </Link>
+          <p className='ml-4'>내 블로그</p>
+        </>
+      ) : (
+        <>
+          <div className='ml-4'>{data?.name}님 반갑습니다.</div>
+          <LogoutButton />
+          <Link href={`/posts/${userId}`} className='ml-4'>
+            내 블로그
+          </Link>
+        </>
+      )}
+      <form action='/search' method='get' className='flex ml-4'>
+        <input className='py-1' type='text' name='q' placeholder='Search...' />
+        <input type='hidden' name='page' value='1' />
+        <input type='hidden' name='limit' value='10' />
+        <button
+          className='text-xl text-white font-bold bg-[#0044ff] hover:bg-[#0000ff] py-1 px-4 m-0'
+          type='submit'
+        >
+          통합검색
+        </button>
+      </form>
+    </div>
   );
 }
 
