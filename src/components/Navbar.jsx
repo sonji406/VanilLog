9c618a22494dcf38ff32d9f6e12ad68216be54f3
Josh5519 <AcForCompany@gmail.com>
2023-08-21 02:22:43 +0900

    Feat: 로고 아이콘으로 변경, 왼쪽 햄버거 버튼 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { LogoutButton } from './LogoutButton';
 
 function Navbar() {
@@ -13,37 +14,55 @@ function Navbar() {
   }
 
   return (
-    <div className='flex justify-between items-center'>
-      <Link href='/'>
-        <span className='text-5xl text-logo font-bold'>vanilLog</span>
-      </Link>
-      {status === 'unauthenticated' ? (
-        <>
-          <Link href='/auth/login' className='ml-4'>
-            로그인
-          </Link>
-          <p className='ml-4'>내 블로그</p>
-        </>
-      ) : (
-        <>
-          <div className='ml-4'>{data?.name}님 반갑습니다.</div>
-          <LogoutButton />
-          <Link href={`/posts/${userId}`} className='ml-4'>
-            내 블로그
-          </Link>
-        </>
-      )}
-      <form action='/search' method='get' className='flex ml-4'>
-        <input className='py-1' type='text' name='q' placeholder='Search...' />
-        <input type='hidden' name='page' value='1' />
-        <input type='hidden' name='limit' value='10' />
-        <button
-          className='text-xl text-white font-bold bg-[#0044ff] hover:bg-[#0000ff] py-1 px-4 m-0'
-          type='submit'
-        >
-          통합검색
-        </button>
-      </form>
+    <div>
+      <div className='flex justify-between items-center border-b-4'>
+        <Link href='/'>
+          <Image
+            src='/image/blogLogo.png'
+            alt='blogLogo'
+            width={40}
+            height={40}
+          ></Image>
+        </Link>
+        {status === 'unauthenticated' ? (
+          <>
+            <Link href='/auth/login' className='ml-4'>
+              로그인
+            </Link>
+            <p className='ml-4'>내 블로그</p>
+          </>
+        ) : (
+          <>
+            <div className='ml-4'>{data?.name}님 반갑습니다.</div>
+            <LogoutButton />
+            <Link href={`/posts/${userId}`} className='ml-4'>
+              내 블로그
+            </Link>
+          </>
+        )}
+        <form action='/search' method='get' className='flex ml-4'>
+          <input
+            className='py-1'
+            type='text'
+            name='q'
+            placeholder='Search...'
+          />
+          <input type='hidden' name='page' value='1' />
+          <input type='hidden' name='limit' value='10' />
+          <button
+            className='text-xl text-white font-bold bg-[#0044ff] hover:bg-[#0000ff] py-1 px-4 m-0'
+            type='submit'
+          >
+            통합검색
+          </button>
+        </form>
+      </div>
+      <Image
+        src='/image/sidebar.png'
+        alt='sidebar'
+        width={40}
+        height={40}
+      ></Image>
     </div>
   );
 }
