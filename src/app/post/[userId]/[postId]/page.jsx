9a47660a19422067c0e1f6f4b0bc64c206a1ca69
Josh5519 <AcForCompany@gmail.com>
2023-08-22 22:30:10 +0900

    Feat: 포스트 상세화면 오류 수정 및 동적 메타데이터 생성

--- src/app/post/[userId]/[postId]/page.jsx
+++ src/app/post/[userId]/[postId]/page.jsx
@@ -1,37 +1,39 @@
 import PostDetail from '@src/components/PostDetail/PostDetail';
+import { METAINFO } from '@utils/metaInfo';
 import axios from 'axios';
-import Error from 'next/error';
 
-// export async function generateMetadata({ params }) {
-//   const postId = params.postId;
-//   try {
-//     const response = await axios.get(`/api/v1/posts/${postId}`);
-//     if (response.data.status !== 'success') {
-//       throw new Error('포스트 데이터 가져오는데 실패하였습니다.');
-//     }
-//     const post = response.data.data;
+export async function generateMetadata({ params }) {
+  const postId = params.postId;
+  try {
+    const response = await axios(
+      `${process.env.DOMAIN}/api/v1/posts/${postId}`,
+    );
+    const post = response.data.data;
+    const blockWithText = post.content.blocks.find((block) => block.data?.text);
 
-//     return {
-//       title: post.title,
-//       // TODO 콘텐트가 하나로 뭉쳐져 있어서 여기서 글을 어떻게 가져와야할지 상의 필요
-//     };
-//   } catch (error) {
-//     return {
-//       title: '포스트 상세화면입니다',
-//       description: '컨텐츠 정보 못 가져왔을시 나오는 메시지',
-//     };
-//   }
-// }
+    const postDescription = blockWithText
+      ? blockWithText.data.text
+      : post.title;
 
-export default function PostDetailPage({}) {
-  // const userId = params.userId;
-  // const postId = params.postId;
-  console.log('postdetailpage');
+    return {
+      title: post.title,
+      description: postDescription,
+    };
+  } catch (error) {
+    return {
+      title: METAINFO.POST_DETAIL.TITLE,
+      description: METAINFO.POST_DETAIL.DESCRIPTION,
+    };
+  }
+}
+
+export default function PostDetailPage({ params }) {
+  const userId = params.userId;
+  const postId = params.postId;
 
   return (
     <div className='bg-gray-100 min-h-screen p-10'>
-      {/* <PostDetail userId={userId} postId={postId} /> */}
-      <p></p>
+      <PostDetail userId={userId} postId={postId} />
     </div>
   );
 }
