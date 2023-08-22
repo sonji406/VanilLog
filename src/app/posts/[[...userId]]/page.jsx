70420ab2492ecf50b499f3bfdde07092e7af4b12
Josh5519 <AcForCompany@gmail.com>
2023-08-22 22:29:32 +0900

    Feat: 블로그 메인화면 동적인 메타데이터 적용

--- src/app/posts/[[...userId]]/page.jsx
+++ src/app/posts/[[...userId]]/page.jsx
@@ -1,11 +1,24 @@
 import { PostList } from '@src/components/Postlist/PostList';
+import { METAINFO } from '@utils/metaInfo';
+import axios from 'axios';
 
-export const metadata = {
-  title: '블로그 메인화면',
-  description: '설명 추가 필요',
-};
+export async function generateMetadata({ params }) {
+  const userId = params.userId ? params.userId[0] : '';
+  try {
+    const user = await axios(`${process.env.DOMAIN}/api/v1/profile/${userId}`);
+    const userData = user.data.data;
 
-//TODO 설명 추가하기
+    return {
+      title: user.data.nickname,
+      description: `${userData.nickname}님의 블로그입니다. 포스트 수 ${userData.blogPosts?.length}`,
+    };
+  } catch (error) {
+    return {
+      title: METAINFO.BLOG_HOME.TITLE,
+      description: METAINFO.BLOG_HOME.DESCRIPTION,
+    };
+  }
+}
 
 export default function Posts({ params }) {
   const blogUserId = params.userId ? params.userId[0] : '';
