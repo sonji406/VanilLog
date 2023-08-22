efd2b148f2abb9f8d849cb8b96fe4a5b55281932
Josh5519 <AcForCompany@gmail.com>
2023-08-22 20:12:01 +0900

    Chore: 임시 저장(오류)

--- src/app/post/[userId]/[postId]/page.jsx
+++ src/app/post/[userId]/[postId]/page.jsx
@@ -1,62 +1,37 @@
-'use client';
+import PostDetail from '@src/components/PostDetail/PostDetail';
+import axios from 'axios';
+import Error from 'next/error';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-import { usePost } from '@utils/usePost';
-import { useComments } from '@utils/useComment';
-import ErrorMessage from '@src/components/PostDetail/ErrorMessage';
-import DeleteModal from '@src/components/PostDetail/DeleteModal';
-import PostContent from '@src/components/PostDetail/PostContent';
-import PostActions from '@src/components/PostDetail/PostActions';
-import CommentsSection from '@src/components/PostDetail/CommentsSection';
+// export async function generateMetadata({ params }) {
+//   const postId = params.postId;
+//   try {
+//     const response = await axios.get(`/api/v1/posts/${postId}`);
+//     if (response.data.status !== 'success') {
+//       throw new Error('포스트 데이터 가져오는데 실패하였습니다.');
+//     }
+//     const post = response.data.data;
 
-export default function PostDetailPage({ params }) {
-  const userId = params.userId;
-  const postId = params.postId;
-  const { data: session } = useSession();
+//     return {
+//       title: post.title,
+//       // TODO 콘텐트가 하나로 뭉쳐져 있어서 여기서 글을 어떻게 가져와야할지 상의 필요
+//     };
+//   } catch (error) {
+//     return {
+//       title: '포스트 상세화면입니다',
+//       description: '컨텐츠 정보 못 가져왔을시 나오는 메시지',
+//     };
+//   }
+// }
 
-  const { post, errorMessage: postError, handleDelete } = usePost(postId);
-  const { errorMessage: commentError } = useComments(postId);
-
-  const router = useRouter();
-  const [showModal, setShowModal] = useState(false);
-
-  const errorMessage = postError || commentError;
-
-  if (!post) return <div>포스트를 불러오는 중...</div>;
+export default function PostDetailPage({}) {
+  // const userId = params.userId;
+  // const postId = params.postId;
+  console.log('postdetailpage');
 
   return (
     <div className='bg-gray-100 min-h-screen p-10'>
-      {errorMessage && (
-        <ErrorMessage
-          message={errorMessage}
-          onClose={() => setErrorMessage(null)}
-        />
-      )}
-
-      {showModal && (
-        <DeleteModal
-          isOpen={showModal}
-          onClose={() => setShowModal(false)}
-          onConfirm={() => handleDelete(router, userId)}
-        />
-      )}
-
-      <div className='bg-white rounded-lg shadow-xl p-8 mx-auto max-w-3xl'>
-        <PostContent title={post.title} content={post.content} />
-
-        {userId === session?.mongoId && (
-          <PostActions
-            userId={userId}
-            session={session}
-            postId={postId}
-            onShowDeleteModal={() => setShowModal(true)}
-          />
-        )}
-
-        <CommentsSection comments={post.comments} />
-      </div>
+      {/* <PostDetail userId={userId} postId={postId} /> */}
+      <p></p>
     </div>
   );
 }
