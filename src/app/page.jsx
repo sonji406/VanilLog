95d5bbd26ee18e421e1c848d73d9cf18d2d46cf2
Josh5519 <AcForCompany@gmail.com>
2023-08-22 17:27:16 +0900

    Chore: 메타데이터 추가

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,9 @@
-'use client';
-
 import { PostList } from '@src/components/Postlist/PostList';
+
+export const metadata = {
+  title: 'VanilLog Home',
+  description: '바닐로그 설명추가...',
+};
 
 export default function Home() {
   return (
--- src/app/posts/[[...userId]]/page.jsx
+++ src/app/posts/[[...userId]]/page.jsx
@@ -1,6 +1,11 @@
-'use client';
-
 import { PostList } from '@src/components/Postlist/PostList';
+
+export const metadata = {
+  title: '블로그 메인화면',
+  description: '설명 추가 필요',
+};
+
+//TODO 설명 추가하기
 
 export default function Posts({ params }) {
   const blogUserId = params.userId ? params.userId[0] : '';
