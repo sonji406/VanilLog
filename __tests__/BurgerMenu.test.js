3f1cfb36777ac44284d1d4f09927951a4b823e22
hamsterster <xhrrl003@gmail.com>
2023-08-25 05:32:14 +0900

    Feat: 버거메뉴 유닛 테스트 구현

---
+++ __tests__/BurgerMenu.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BurgerMenu } from '@src/components/SideNavbar/BurgerMenu';
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('<BurgerMenu />', () => {
+  it('isOpen이 false일 때 open 클래스가 없어야 한다.', () => {
+    const { container } = render(<BurgerMenu isOpen={false} />);
+    const div = container.firstChild;
+
+    expect(div.className).toBe('w-5 h-5 flex flex-col justify-between ');
+  });
+
+  it('isOpen이 true일 때 open 클래스가 있어야 한다.', () => {
+    const { container } = render(<BurgerMenu isOpen={true} />);
+    const div = container.firstChild;
+
+    expect(div.className).toBe('w-5 h-5 flex flex-col justify-between open');
+  });
+});
