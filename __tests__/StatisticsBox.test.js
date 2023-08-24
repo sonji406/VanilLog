788b66beeb4dbe903f3b3289881fad1d0025747f
hamsterster <xhrrl003@gmail.com>
2023-08-25 05:31:49 +0900

    Refactor: 사이드바의 통계 부분 유닛 테스트 수정

--- __tests__/StatisticsBox.test.js
+++ __tests__/StatisticsBox.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { StatisticsBox } from '@src/components/SideNavbar/StatisticsBox';
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('<StatisticsBox />', () => {
-  it('일일 방문자 수와 총 방문자 수 렌더링 테스트', () => {
+  it('일일 방문자 수와 총 방문자 수가 올바르게 렌더링되어야 한다.', () => {
     render(<StatisticsBox />);
 
     expect(screen.getByText(/일일 방문자: 1명/i)).toBeInTheDocument();
