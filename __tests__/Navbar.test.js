0e0ad6c849dc2de8a5e1270860d45188dc9fc705
hamsterster <xhrrl003@gmail.com>
2023-08-25 05:35:22 +0900

    Feat: Navbar 컴포넌트 유닛 테스트 구현

--- __tests__/Navbar.test.js
+++ __tests__/Navbar.test.js
@@ -5,7 +5,9 @@ import { usePathname } from 'next/navigation';
 import { Navbar } from '@src/components/Navbar';
 
 beforeEach(() => {
+  jest.clearAllMocks();
   useSession.mockReturnValue({ status: 'loading' });
+  usePathname.mockImplementation(() => '/some/path');
 });
 
 describe('<Navbar />', () => {
@@ -13,7 +15,7 @@ describe('<Navbar />', () => {
     usePathname.mockImplementation(() => '/some/path');
   });
 
-  it('로딩 상태에서 "Loading..." 텍스트가 나타나는지 테스트', () => {
+  it('로딩 상태에서 "Loading..." 텍스트가 나타나야 한다.', () => {
     useSession.mockReturnValue({ status: 'loading' });
 
     render(<Navbar />);
@@ -21,7 +23,7 @@ describe('<Navbar />', () => {
     expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
 
-  it('비인증 상태에서 "로그인"과 "내 블로그" 텍스트가 나타나는지 테스트', () => {
+  it('비인증 상태에서 "로그인"과 "내 블로그" 텍스트가 나타나야 한다.', () => {
     useSession.mockReturnValue({ status: 'unauthenticated' });
 
     render(<Navbar />);
@@ -30,7 +32,7 @@ describe('<Navbar />', () => {
     expect(screen.getByText('내 블로그')).toBeInTheDocument();
   });
 
-  it('인증된 상태에서 사용자 이름과 로그아웃 버튼이 나타나는지 테스트', () => {
+  it('인증된 상태에서 사용자 이름과 로그아웃 버튼이 나타나야 한다.', () => {
     useSession.mockReturnValue({
       status: 'authenticated',
       data: { name: '우지현', mongoId: '123abc' },
@@ -41,7 +43,7 @@ describe('<Navbar />', () => {
     expect(screen.getByText('우지현님 반갑습니다.')).toBeInTheDocument();
   });
 
-  it('검색 칸이 올바르게 렌더링되는지 테스트', () => {
+  it('검색 칸이 올바르게 렌더링되어야 한다.', () => {
     useSession.mockReturnValue({ status: 'unauthenticated' });
 
     render(<Navbar />);
