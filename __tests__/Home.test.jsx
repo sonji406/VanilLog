421b6456337c34ec88d9282542b9625fc5ee79b4
hamsterster <xhrrl003@gmail.com>
2023-08-26 11:34:22 +0900

    Feat: 홈 page 유닛 테스트 구현

---
+++ __tests__/Home.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import Home from '@src/app/page';
+
+jest.mock('@src/components/PostList/PostList', () => {
+  return {
+    __esModule: true,
+    PostList: () => <div>Mocked Post List</div>,
+  };
+});
+
+describe('<Home />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('포스트 리스트 컴포넌트가 정상적으로 렌더링되어야 한다.', async () => {
+    await act(async () => {
+      render(<Home />);
+    });
+
+    expect(screen.getByText('Mocked Post List')).toBeInTheDocument();
+  });
+});
