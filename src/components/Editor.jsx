3e8288e4fe951fa85cff767f8967fa9170fb7aee
hamsterster <xhrrl003@gmail.com>
2023-08-27 12:18:33 +0900

    Chore: 기타 코드 수정

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import EditorJS from '@editorjs/editorjs';
