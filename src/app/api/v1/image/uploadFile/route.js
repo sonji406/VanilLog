5f8850520243eb442117f2834183e33312451f35
Josh5519 <AcForCompany@gmail.com>
2023-09-03 20:26:40 +0900

    Chore: 이미지 api 주석 빠진 부분 추가

--- src/app/api/v1/image/uploadFile/route.js
+++ src/app/api/v1/image/uploadFile/route.js
@@ -14,6 +14,11 @@ const s3Data = new S3({
 
 const bucketName = process.env.AWS_S3_BUCKET_NAME;
 
+/**
+ * 이미지 조회 api
+ * @URL /api/v1/image/uploadFile
+ * @param request
+ */
 async function GET(request) {
   try {
     const { file: fileName, fileType } = request.query;
@@ -50,6 +55,11 @@ async function GET(request) {
   }
 }
 
+/**
+ * 이미지 업로드 api
+ * @URL /api/v1/image/uploadFile
+ * @param request
+ */
 async function POST(request) {
   try {
     const formData = await request.formData();
