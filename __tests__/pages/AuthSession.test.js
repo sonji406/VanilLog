ab945b2324a60bc3c463dd3e6c2158659da8b2b4
hamsterster <xhrrl003@gmail.com>
2023-10-23 17:18:38 +0900

    Chore: AuthSession 테스트 코드 내 텍스트 일관성 수정

--- __tests__/pages/AuthSession.test.js
+++ __tests__/pages/AuthSession.test.js
@@ -19,7 +19,7 @@ describe('<AuthSession />', () => {
     jest.clearAllMocks();
   });
 
-  it('Navbar, SideNavbar 컴포넌트와 자식 컴포넌트가 올바르게 렌더링되어야 한다.', async () => {
+  it('Navbar, SideNavbar 컴포넌트와 자식 컴포넌트가 올바르게 렌더링 되어야 한다.', async () => {
     await act(async () => {
       render(
         <AuthSession>
