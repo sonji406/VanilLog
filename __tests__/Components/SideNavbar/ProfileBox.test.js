1ca98243186677399899dd1f251bf92915ae4b0a
hamsterster <xhrrl003@gmail.com>
2023-10-23 17:17:56 +0900

    Chore: ProfileBox 테스트 코드 내 텍스트 일관성 수정

--- __tests__/Components/SideNavbar/ProfileBox.test.js
+++ __tests__/Components/SideNavbar/ProfileBox.test.js
@@ -1,37 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import { ProfileBox } from '@src/components/SideNavbar/ProfileBox';
 
-describe('ProfileBox Component', () => {
-  it('displays an error message when an error is present', () => {
-    render(<ProfileBox profile={{}} error='Sample error message' />);
+describe('<ProfileBox />', () => {
+  it('에러가 발생하였을 때 에러 메시지가 렌더링 되어야 합니다.', () => {
+    render(<ProfileBox profile={{}} error='test error message' />);
 
-    expect(screen.getByText('Sample error message')).toBeInTheDocument();
+    expect(screen.getByText('test error message')).toBeInTheDocument();
   });
 
-  it('displays the profile image when provided', () => {
-    const mockImage = '/sample-image.jpg';
-    render(<ProfileBox profile={{ profileImage: mockImage }} error={null} />);
-
-    expect(screen.getByAltText('프로필 사진')).toBeInTheDocument();
-  });
-
-  it('displays login button when no profile image and no error', () => {
+  it('프로필 이미지가 없고 에러도 없을 때에는 로그인 버튼이 렌더링 되어야 합니다.', () => {
     render(<ProfileBox profile={{}} error={null} />);
 
     expect(screen.getByText('로그인을 해주세요.')).toBeInTheDocument();
   });
 
-  it('displays nickname and profile info/edit button when userId is provided', () => {
-    const mockUserId = 'sampleUserId';
-    const mockNickname = 'sampleNickname';
+  it('사용자 ID가 제공되면 사용자의 프로필 사진, 닉네임, 프로필 정보/수정 버튼이 렌더링 되어야 합니다.', () => {
+    const mockUserId = 'testUserId';
+    const mockNickname = 'testNickname';
+    const mockImage = '/test-image.jpg';
 
     render(
       <ProfileBox
-        profile={{ _id: mockUserId, nickname: mockNickname }}
+        profile={{
+          _id: mockUserId,
+          profileImage: mockImage,
+          nickname: mockNickname,
+        }}
         error={null}
       />,
     );
 
+    expect(screen.getByAltText('프로필 사진')).toBeInTheDocument();
     expect(screen.getByText(mockNickname)).toBeInTheDocument();
     expect(screen.getByText('프로필 정보/수정')).toBeInTheDocument();
   });
