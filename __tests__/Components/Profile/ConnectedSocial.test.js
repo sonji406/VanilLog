ef5f57bea66a83a7909104246ba0e188af475c67
hamsterster <xhrrl003@gmail.com>
2023-10-23 17:17:16 +0900

    Chore: ConnectedSocial 테스트 코드 내 텍스트 일관성 수정

--- __tests__/Components/Profile/ConnectedSocial.test.js
+++ __tests__/Components/Profile/ConnectedSocial.test.js
@@ -3,13 +3,13 @@ import { render } from '@testing-library/react';
 import ConnectedSocial from '@src/components/Profile/ConnectedSocial';
 
 const mockUserData = {
-  _id: '60af5a48bbed8635907d8f78',
-  nickname: 'testUser',
+  _id: 'testUserId',
+  nickname: 'testUserNickname',
   email: 'test@test.com',
   socialLoginType: 'google',
   profileImage: 'https://example.com/image.jpg',
-  blogPosts: ['60af5a48bbed8635907d8f79'],
-  comments: ['60af5a48bbed8635907d8f80'],
+  blogPosts: ['blogId'],
+  comments: ['commentId'],
 };
 
 describe('<ConnectedSocial />', () => {
@@ -17,11 +17,11 @@ describe('<ConnectedSocial />', () => {
     jest.clearAllMocks();
   });
 
-  it('should render without crashing', () => {
+  it('연결된 소셜 컴포넌트가 올바르게 렌더링 되어야 합니다.', () => {
     render(<ConnectedSocial socialLoginType={mockUserData.socialLoginType} />);
   });
 
-  it('should display the correct socialLoginType', () => {
+  it('올바른 socialLoginType이 표시되어야 합니다.', () => {
     const { getByText } = render(
       <ConnectedSocial socialLoginType={mockUserData.socialLoginType} />,
     );
