7106e6635f923dab4e25ce59a8557bab86512d2d
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:56:20 +0900

    Refactor: PostList 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/Posts/PostList.test.js
+++ __tests__/Components/Posts/PostList.test.js
@@ -5,45 +5,35 @@ import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import axios from 'axios';
 
-jest.mock('next/router', () => ({
-  useRouter: jest.fn(),
-}));
+const mockAxiosGet = (data) => {
+  axios.get.mockResolvedValue({
+    data: {
+      status: 200,
+      ...data,
+    },
+  });
+};
 
-jest.mock('axios');
-
-jest.mock('next-auth/react', () => ({
-  useSession: jest.fn(),
-}));
-
-describe('<PostList /> - Pagination', () => {
+const renderPostListWithQuery = (query) => {
   const mockRouter = {
     push: jest.fn(),
-    query: {
-      q: null,
-      page: null,
-      limit: null,
-    },
+    query,
   };
 
-  beforeEach(() => {
-    useRouter.mockReturnValue(mockRouter);
-    useSession.mockReturnValue({ data: null });
-  });
+  useRouter.mockReturnValue(mockRouter);
+  useSession.mockReturnValue({ data: null });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  return render(<PostList blogUserId={null} />);
+};
 
+describe('<PostList /> - Pagination', () => {
   it('페이지 넘버 1 버튼이 표시될 때 그 버튼은 올바른 href로 렌더링 되어야 한다.', async () => {
-    axios.get.mockResolvedValue({
-      data: {
-        status: 200,
-        data: [],
-        totalPosts: 10,
-      },
+    mockAxiosGet({
+      data: [],
+      totalPosts: 10,
     });
 
-    render(<PostList blogUserId={null} />);
+    renderPostListWithQuery({ page: '1', limit: '10' });
 
     await waitFor(() => expect(screen.getByText('1')).toBeInTheDocument());
 
@@ -52,15 +42,12 @@ describe('<PostList /> - Pagination', () => {
   });
 
   it('페이지 넘버 2 버튼이 표시될 때 그 버튼은 올바른 href로 렌더링 되어야 한다.', async () => {
-    axios.get.mockResolvedValue({
-      data: {
-        status: 200,
-        data: [],
-        totalPosts: 50,
-      },
+    mockAxiosGet({
+      data: [],
+      totalPosts: 50,
     });
 
-    render(<PostList blogUserId={null} />);
+    renderPostListWithQuery({ page: '2', limit: '10' });
 
     await waitFor(() => expect(screen.getByText('2')).toBeInTheDocument());
 
@@ -70,26 +57,8 @@ describe('<PostList /> - Pagination', () => {
 });
 
 describe('<PostList /> - Search Results', () => {
-  const mockRouter = {
-    push: jest.fn(),
-    query: {
-      q: 'searchQuery',
-      page: null,
-      limit: null,
-    },
-  };
-
-  beforeEach(() => {
-    useRouter.mockReturnValue(mockRouter);
-    useSession.mockReturnValue({ data: null });
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   it('검색어에 해당되는 포스트가 있는 경우 검색 결과가 렌더링 되어야 한다.', async () => {
-    const mockPosts = [
+    const mockSearchResults = [
       {
         _id: 'testPostId1',
         author: 'testAuthorId1',
@@ -104,15 +73,12 @@ describe('<PostList /> - Search Results', () => {
       },
     ];
 
-    axios.get.mockResolvedValue({
-      data: {
-        status: 200,
-        data: mockPosts,
-        totalPosts: mockPosts.length,
-      },
+    mockAxiosGet({
+      data: mockSearchResults,
+      totalPosts: mockSearchResults.length,
     });
 
-    render(<PostList blogUserId={null} />);
+    renderPostListWithQuery({ q: 'searchQuery' });
 
     await waitFor(() => {
       expect(screen.getByText('Search Result 1')).toBeInTheDocument();
@@ -121,15 +87,12 @@ describe('<PostList /> - Search Results', () => {
   });
 
   it('검색어에 해당되는 포스트가 없는 경우 검색 결과가 없다는 텍스트가 렌더링 되어야 한다.', async () => {
-    axios.get.mockResolvedValue({
-      data: {
-        status: 200,
-        data: [],
-        totalPosts: 0,
-      },
+    mockAxiosGet({
+      data: [],
+      totalPosts: 0,
     });
 
-    render(<PostList blogUserId={null} />);
+    renderPostListWithQuery({ q: 'searchQuery' });
 
     await waitFor(() => {
       expect(screen.getByText('검색 결과가 없습니다.')).toBeInTheDocument();
