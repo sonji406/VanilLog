57d0c4a847e5e86affc9175b2fe0d1e129e65453
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:58:55 +0900

    Refactor: Profile Page 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/pages/ProfilePage.test.js
+++ __tests__/pages/ProfilePage.test.js
@@ -6,62 +6,73 @@ import { useUserProfile } from '@utils/useUserProfile';
 import { useImageUpload } from '@utils/useImageUpload';
 import { useNicknameUpdate } from '@utils/useNicknameUpdate';
 
+const renderProfilePage = async (params) => {
+  await act(async () => {
+    render(<ProfilePage params={params} />);
+  });
+};
+
+const mockUseSession = (returnValue) => {
+  useSession.mockReturnValue(returnValue);
+};
+
+const mockUseUserProfile = (returnValue) => {
+  useUserProfile.mockReturnValue(returnValue);
+};
+
+const mockUseImageUpload = (returnValue) => {
+  useImageUpload.mockReturnValue(returnValue);
+};
+
+const mockUseNicknameUpdate = (returnValue) => {
+  useNicknameUpdate.mockReturnValue(returnValue);
+};
+
 describe('<ProfilePage />', () => {
   it('프로필 데이터가 올바르게 렌더링 되어야 한다.', async () => {
-    useSession.mockReturnValue({
-      data: { session: { mongoId: 'testUserId' } },
-    });
-
-    useUserProfile.mockReturnValue({
+    mockUseSession({ data: { session: { mongoId: 'testUserId' } } });
+    mockUseUserProfile({
       userProfile: { nickname: 'testUserNickname', socialLoginType: 'google' },
       loading: false,
       error: null,
     });
-
-    useImageUpload.mockReturnValue({
+    mockUseImageUpload({
       uploadedImage: null,
       handleImageUpload: jest.fn(),
     });
-
-    useNicknameUpdate.mockReturnValue({
+    mockUseNicknameUpdate({
       nickname: 'testUserNickname',
       setNickname: jest.fn(),
       message: '',
       updateNickname: jest.fn(),
     });
 
-    await act(async () => {
-      render(<ProfilePage params={{ userId: 'testUserId' }} />);
-    });
+    await renderProfilePage({ userId: 'testUserId' });
 
     expect(screen.getByText('testUserNickname')).toBeInTheDocument();
     expect(screen.getByText('google')).toBeInTheDocument();
   });
 
   it('로딩 상태가 올바르게 렌더링 되어야 한다.', async () => {
-    useUserProfile.mockReturnValue({
+    mockUseUserProfile({
       userProfile: null,
       loading: true,
       error: null,
     });
 
-    await act(async () => {
-      render(<ProfilePage params={{ userId: 'testUserId' }} />);
-    });
+    await renderProfilePage({ userId: 'testUserId' });
 
     expect(screen.getByText('프로필을 불러오는 중입니다.')).toBeInTheDocument();
   });
 
   it('에러 상태를 올바르게 처리해야 한다.', async () => {
-    useUserProfile.mockReturnValue({
+    mockUseUserProfile({
       userProfile: null,
       loading: false,
       error: '에러 발생',
     });
 
-    await act(async () => {
-      render(<ProfilePage params={{ userId: 'testUserId' }} />);
-    });
+    await renderProfilePage({ userId: 'testUserId' });
 
     expect(screen.getByText('Error: 에러 발생')).toBeInTheDocument();
   });
