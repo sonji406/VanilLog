b4d081a88ac408c225302289ac3b4a4ab4002660
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:57:40 +0900

    Refactor: SideNavbar 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/SideNavbar/SideNavbar.test.js
+++ __tests__/Components/SideNavbar/SideNavbar.test.js
@@ -4,95 +4,82 @@ import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { SideNavbar } from '@src/components/SideNavbar/SideNavbar';
 
-jest.mock('axios');
-jest.mock('next-auth/react');
-jest.mock('next/navigation');
+const renderSideNavbar = () => render(<SideNavbar />);
+
+const mockResolvedProfile = () => {
+  axios.get.mockResolvedValueOnce({
+    data: {
+      status: 200,
+      data: { _id: 'testUserId', nickname: 'testUserNickname' },
+    },
+  });
+};
+
+const mockRejectedProfile = () => {
+  axios.get.mockRejectedValueOnce(new Error());
+};
+
+beforeEach(() => {
+  usePathname.mockReturnValue('/home');
+  useSession.mockReturnValue({ data: {} });
+});
 
 describe('<SideNavbar />', () => {
-  beforeEach(() => {
-    usePathname.mockReturnValue('/home');
-  });
-
   it('userId가 존재할 때 프로필 데이터를 가져와야 한다.', async () => {
     useSession.mockReturnValue({ data: { mongoId: 'testUserId' } });
-    axios.get.mockResolvedValueOnce({
-      data: {
-        status: 200,
-        data: { _id: 'testUserId', nickname: 'testUserNickname' },
-      },
-    });
+    mockResolvedProfile();
 
-    render(<SideNavbar />);
+    renderSideNavbar();
 
-    await waitFor(() => {
-      expect(screen.getByText('testUserNickname')).toBeInTheDocument();
-    });
+    expect(await screen.findByText('testUserNickname')).toBeInTheDocument();
   });
 
   it('API 에러가 있을 경우 에러 메시지가 렌더링 되어야 한다.', async () => {
     useSession.mockReturnValue({ data: { mongoId: 'testUserId' } });
-    axios.get.mockRejectedValueOnce(new Error());
+    mockRejectedProfile();
 
-    render(<SideNavbar />);
+    renderSideNavbar();
 
-    await waitFor(() => {
-      expect(
-        screen.getByText('프로필을 불러오는 중 문제가 발생했습니다.'),
-      ).toBeInTheDocument();
-    });
+    expect(
+      await screen.findByText('프로필을 불러오는 중 문제가 발생했습니다.'),
+    ).toBeInTheDocument();
   });
 
   it('비로그인 시, 사이드바 메뉴에 로그인 버튼이 렌더링 되어야 한다.', async () => {
-    useSession.mockReturnValue({ data: {} });
-
-    render(<SideNavbar />);
+    renderSideNavbar();
 
     const menuButton = screen.getByTitle('menu');
     fireEvent.mouseEnter(menuButton);
 
-    await waitFor(() => {
-      expect(screen.getByText('로그인을 해주세요.')).toBeVisible();
-    });
+    expect(await screen.findByText('로그인을 해주세요.')).toBeVisible();
   });
 
   it('로그인 시, 사이드바 메뉴에 사용자의 닉네임이 렌더링 되어야 한다.', async () => {
     useSession.mockReturnValue({ data: { mongoId: 'testUserId' } });
-    axios.get.mockResolvedValueOnce({
-      data: {
-        status: 200,
-        data: { _id: 'testUserId', nickname: 'testUserNickname' },
-      },
-    });
+    mockResolvedProfile();
 
-    render(<SideNavbar />);
+    renderSideNavbar();
 
     const menuButton = screen.getByTitle('menu');
     fireEvent.mouseEnter(menuButton);
 
-    await waitFor(() => {
-      expect(screen.getByText('testUserNickname')).toBeVisible();
-    });
+    expect(await screen.findByText('testUserNickname')).toBeVisible();
   });
 
-  it('사이드바에서 마우스를 떼면 사이드바가 닫혀야 한다.', async () => {
-    useSession.mockReturnValue({ data: {} });
-
-    render(<SideNavbar />);
+  it('사이드바에서 마우스를 떼면 사이드바가 닫혀야 한다.', () => {
+    renderSideNavbar();
 
     const sidebarMenu = screen.getByRole('navigation');
     fireEvent.mouseEnter(sidebarMenu);
     fireEvent.mouseLeave(sidebarMenu);
 
-    await waitFor(() => {
-      expect(screen.queryByText('testUserNickname')).not.toBeInTheDocument();
-    });
+    expect(screen.queryByText('testUserNickname')).not.toBeInTheDocument();
   });
 
   it('프로필 페이지에서는 사이드바가 렌더링 되지 않아야 한다.', () => {
-    useSession.mockReturnValue({ data: {} });
     usePathname.mockReturnValue('/profile/sample');
 
-    render(<SideNavbar />);
+    renderSideNavbar();
 
     expect(screen.queryByRole('navigation')).toBeNull();
   });
