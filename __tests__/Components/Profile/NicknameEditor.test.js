973b9746820573af385f66318892ed1216e48f11
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:57:00 +0900

    Refactor: NicknameEditor 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/Profile/NicknameEditor.test.js
+++ __tests__/Components/Profile/NicknameEditor.test.js
@@ -3,87 +3,50 @@ import { render, fireEvent, act } from '@testing-library/react';
 import NicknameEditor from '@src/components/Profile/NicknameEditor';
 
 const mockUserData = {
-  _id: 'testUserId',
   nickname: 'testUserNickname',
-  email: 'test@test.com',
-  socialLoginType: 'google',
 };
 
 describe('<NicknameEditor />', () => {
+  let toggleEditing, setNickname, updateNickname;
+
   beforeEach(() => {
-    jest.clearAllMocks();
+    toggleEditing = jest.fn();
+    setNickname = jest.fn();
+    updateNickname = jest.fn();
   });
 
-  it('닉네임 편집 컴포넌트가 올바르게 렌더링 되어야 한다.', () => {
+  const renderNicknameEditor = (props) =>
     render(
-      <NicknameEditor
-        editing={false}
-        toggleEditing={() => {}}
-        nickname={mockUserData.nickname}
-        setNickname={() => {}}
-        updateNickname={() => {}}
-        message=''
-      />,
-    );
-  });
-
-  it('변경하기 버튼을 클릭하면 편집 가능한 상태로 변경되어야 한다.', () => {
-    const toggleEditing = jest.fn();
-    const { getByText, queryByRole } = render(
       <NicknameEditor
         editing={false}
         toggleEditing={toggleEditing}
         nickname={mockUserData.nickname}
-        setNickname={() => {}}
-        updateNickname={() => {}}
+        setNickname={setNickname}
+        updateNickname={updateNickname}
         message=''
+        {...props}
       />,
     );
 
-    expect(queryByRole('textbox')).toBeNull();
+  it('닉네임 편집 컴포넌트가 올바르게 렌더링 되어야 한다.', () => {
+    const { getByText } = renderNicknameEditor();
 
+    expect(getByText(mockUserData.nickname)).toBeInTheDocument();
+  });
+
+  it('변경하기 버튼을 클릭하면 편집 가능한 상태로 변경되어야 한다.', () => {
+    const { getByText } = renderNicknameEditor();
     const editButton = getByText('변경하기');
-    act(() => {
-      fireEvent.click(editButton);
-    });
+
+    fireEvent.click(editButton);
 
     expect(toggleEditing).toHaveBeenCalled();
   });
 
-  it('닉네임 변경이 완료되면 올바른 메시지가 나타나야 한다.', async () => {
-    let showMessage = '';
-    const setMessage = (newMessage) => {
-      showMessage = newMessage;
-    };
+  it('닉네임 변경이 완료되면 올바른 메시지가 나타나야 한다.', () => {
+    const message = '닉네임이 성공적으로 업데이트되었습니다';
+    const { getByText } = renderNicknameEditor({ message });
 
-    const { rerender, getByText } = render(
-      <NicknameEditor
-        editing={false}
-        toggleEditing={() => {}}
-        nickname={mockUserData.nickname}
-        setNickname={() => {}}
-        updateNickname={() => {}}
-        message=''
-      />,
-    );
-
-    act(() => {
-      setMessage('닉네임이 성공적으로 업데이트되었습니다');
-    });
-
-    rerender(
-      <NicknameEditor
-        editing={false}
-        toggleEditing={() => {}}
-        nickname={mockUserData.nickname}
-        setNickname={() => {}}
-        updateNickname={() => {}}
-        message={showMessage}
-      />,
-    );
-
-    expect(
-      getByText('닉네임이 성공적으로 업데이트되었습니다'),
-    ).toBeInTheDocument();
+    expect(getByText(message)).toBeInTheDocument();
   });
 });
