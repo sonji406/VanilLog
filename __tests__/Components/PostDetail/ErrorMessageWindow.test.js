66a86bc0b8fc4eca9fd3cbdcf504ff368cccc2b8
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:55:32 +0900

    Refactor: ErrorMessageWindow 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/PostDetail/ErrorMessageWindow.test.js
+++ __tests__/Components/PostDetail/ErrorMessageWindow.test.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { render, screen, act, fireEvent } from '@testing-library/react';
 import ErrorMessageWindow from '@src/components/PostDetail/ErrorMessageWindow';
 
+const renderErrorMessageWindow = (message) => {
+  return render(<ErrorMessageWindow message={message} />);
+};
+
 describe('<ErrorMessageWindow />', () => {
   it('에러 메시지가 주어졌을 때 올바르게 렌더링되어야 한다.', () => {
-    render(<ErrorMessageWindow message='에러가 발생하였습니다.' />);
+    renderErrorMessageWindow('에러가 발생하였습니다.');
 
     expect(screen.getByText('에러가 발생하였습니다.')).toBeInTheDocument();
   });
 
   it('에러 메시지가 없을 때 렌더링되지 않아야 한다.', () => {
-    render(<ErrorMessageWindow message={null} />);
+    renderErrorMessageWindow(null);
 
     expect(
       screen.queryByText('에러가 발생하였습니다.'),
@@ -18,7 +22,7 @@ describe('<ErrorMessageWindow />', () => {
   });
 
   it("'X' 버튼 클릭 시 에러 메시지가 사라져야 한다.", () => {
-    render(<ErrorMessageWindow message='에러가 발생하였습니다.' />);
+    renderErrorMessageWindow('에러가 발생하였습니다.');
 
     const closeButton = screen.getByText('X');
 
