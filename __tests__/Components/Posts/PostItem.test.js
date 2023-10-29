b0f06556a4e66d98754e05395ecdc384b2d54ed0
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:56:12 +0900

    Refactor: PostItem 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/Posts/PostItem.test.js
+++ __tests__/Components/Posts/PostItem.test.js
@@ -1,62 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import { PostItem } from '@src/components/Posts/PostItem';
 
-jest.mock('next/legacy/image', () => {
-  const MockedImage = ({ src, alt }) => <img src={src} alt={alt} />;
-  MockedImage.displayName = 'MockedImage';
-  return MockedImage;
-});
+const renderPostItem = (post) => {
+  return render(<PostItem post={post} />);
+};
 
 describe('<PostItem />', () => {
   it('이미지가 있는 포스트의 경우 이미지가 썸네일로 렌더링 되어야 한다.', () => {
-    const mockPostWithImage = {
-      title: 'Test Title',
-      content: {
-        blocks: [
-          {
-            type: 'image',
-            data: { file: { url: 'https://example.com/test.jpg' } },
-          },
-          { type: 'paragraph', data: { text: 'Test Post Content' } },
-        ],
-      },
-    };
+    renderPostItem(global.mockPostData);
 
-    render(<PostItem post={mockPostWithImage} />);
+    const imageBlock = global.mockPostData.content.blocks.find(
+      (block) => block.type === 'image',
+    );
+    const imageUrl = imageBlock.data.file.url;
+    const imageElement = screen.getByRole('img');
 
-    const imageElement = screen.getByRole('img', {
-      name: /Test Post Content/i,
-    });
-    expect(imageElement).toBeInTheDocument();
-    expect(imageElement).toHaveAttribute('src', 'https://example.com/test.jpg');
+    expect(imageElement).toHaveAttribute('alt', 'testParagraphContent');
+    expect(imageElement).toHaveAttribute('src', imageUrl);
   });
 
   it('이미지가 없는 포스트의 경우 랜덤 색상이 썸네일로 렌더링 되어야 한다.', () => {
-    const mockPost = {
-      title: 'Test Title',
+    const mockPostWithoutImage = {
+      ...global.mockPostData,
       content: {
         blocks: [{ type: 'paragraph', data: { text: 'Test Post Content' } }],
       },
     };
 
-    render(<PostItem post={mockPost} />);
+    renderPostItem(mockPostWithoutImage);
 
     const coloredDiv =
       screen.getByText('Test Post Content').parentElement.previousSibling;
+
     expect(coloredDiv).toHaveClass('w-full', 'h-[170px]');
   });
 
   it('제목과 콘텐츠가 올바르게 렌더링 되어야 한다.', () => {
-    const mockPost = {
-      title: 'Test Title',
-      content: {
-        blocks: [{ type: 'paragraph', data: { text: 'Test Post Content' } }],
-      },
-    };
-
-    render(<PostItem post={mockPost} />);
+    renderPostItem(global.mockPostData);
 
     expect(screen.getByText('Test Title')).toBeInTheDocument();
-    expect(screen.getByText('Test Post Content')).toBeInTheDocument();
+    expect(screen.getByText('testParagraphContent')).toBeInTheDocument();
   });
 });
