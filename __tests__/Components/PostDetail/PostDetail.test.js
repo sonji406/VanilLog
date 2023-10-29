cdbc85fb96bc175c081359020d894fba0a74916d
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:56:01 +0900

    Refactor: PostDetail 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/PostDetail/PostDetail.test.js
+++ __tests__/Components/PostDetail/PostDetail.test.js
@@ -5,46 +5,42 @@ import PostDetail from '@src/components/PostDetail/PostDetail';
 import { usePost } from '@utils/usePost';
 import { useComments } from '@utils/useComment';
 
-jest.mock('next/navigation');
-jest.mock('next-auth/react');
-jest.mock('@utils/usePost');
-jest.mock('@utils/useComment');
-
-describe('<PostDetail />', () => {
-  let mockSession, mockPost, mockComments;
-
-  beforeEach(() => {
-    mockSession = useSession.mockReturnValue({
-      data: { mongoId: 'testUserId' },
-    });
-
-    mockPost = usePost.mockReturnValue({ post: global.mockPostData });
-    mockComments = useComments.mockReturnValue({});
-
-    useRouter.mockReturnValue({
-      route: '/post/detail',
-      pathname: '/post/detail',
-      query: {},
-      asPath: '',
-    });
+const renderPostDetail = (props) => {
+  useSession.mockReturnValue({
+    data: { mongoId: 'testUserId' },
   });
 
+  usePost.mockReturnValue({ post: global.mockPostData });
+
+  useComments.mockReturnValue({});
+
+  useRouter.mockReturnValue({
+    route: '/post/detail',
+    pathname: '/post/detail',
+    query: {},
+    asPath: '',
+  });
+
+  return render(<PostDetail {...props} />);
+};
+
+describe('<PostDetail />', () => {
   it('포스트를 불러오는 동안 로딩 상태가 렌더링 되어야 한다.', () => {
-    mockPost.mockReturnValueOnce({ post: null });
-    render(<PostDetail userId='testUserId' postId='testPostId' />);
+    usePost.mockReturnValueOnce({ post: null });
+    renderPostDetail({ userId: 'testUserId', postId: 'testPostId' });
 
     expect(screen.getByText('포스트를 불러오는 중...')).toBeInTheDocument();
   });
 
   it('포스트가 불러와졌을 경우 포스트 내용이 렌더링 되어야 한다.', async () => {
-    render(<PostDetail userId='testUserId' postId='testPostId' />);
+    renderPostDetail({ userId: 'testUserId', postId: 'testPostId' });
     const titleElement = await screen.findByText('Test Title');
 
     expect(titleElement).toBeInTheDocument();
   });
 
   it('세션 ID와 사용자 ID가 일치하는 경우 포스트 수정 및 삭제 기능이 렌더링 되어야 한다.', async () => {
-    render(<PostDetail userId='testUserId' postId='testPostId' />);
+    renderPostDetail({ userId: 'testUserId', postId: 'testPostId' });
     const editAction = await screen.findByText('수정하기');
     const deleteAction = await screen.findByText('삭제하기');
 
@@ -53,8 +49,8 @@ describe('<PostDetail />', () => {
   });
 
   it('세션 ID와 사용자 ID가 일치하지 않는 경우 포스트 수정 및 삭제 기능이 렌더링 되지 않아야 한다.', () => {
-    mockSession.mockReturnValueOnce({ data: { mongoId: 'anotherTestUserId' } });
-    render(<PostDetail userId='testUserId' postId='testPostId' />);
+    useSession.mockReturnValueOnce({ data: { mongoId: 'anotherTestUserId' } });
+    renderPostDetail({ userId: 'testUserId', postId: 'testPostId' });
 
     expect(screen.queryByText('수정하기')).not.toBeInTheDocument();
     expect(screen.queryByText('삭제하기')).not.toBeInTheDocument();
