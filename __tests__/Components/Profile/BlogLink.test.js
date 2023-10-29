5423bc4eee03da8fcf7c2ec090de14023fb082fa
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:56:49 +0900

    Refactor: BlogLink 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/Profile/BlogLink.test.js
+++ __tests__/Components/Profile/BlogLink.test.js
@@ -1,58 +1,60 @@
 import React from 'react';
-import { render, act, fireEvent, waitFor } from '@testing-library/react';
+import { render, act, fireEvent } from '@testing-library/react';
 import BlogLink from '@src/components/Profile/BlogLink';
 
-global.navigator.clipboard = {
-  writeText: jest.fn(() => Promise.resolve()),
+jest.useFakeTimers();
+
+const mockUserData = {
+  sessionId: 'testUserId',
 };
 
+const blogLinkUrl = `https://vanillog/posts/${mockUserData.sessionId}`;
+
+const successMessage = '내 블로그 링크 복사 완료!';
+
+const renderBlogLink = () =>
+  render(<BlogLink sessionId={mockUserData.sessionId} />);
+
 describe('<BlogLink />', () => {
-  let mockData;
+  let getByText, queryByText, findByText;
 
   beforeEach(() => {
-    mockData = {
-      sessionId: 'testUserId',
-    };
-    jest.clearAllMocks();
+    const renderResult = renderBlogLink();
+    getByText = renderResult.getByText;
+    queryByText = renderResult.queryByText;
+    findByText = renderResult.findByText;
   });
 
   it('블로그 링크 컴포넌트가 올바르게 렌더링 되어야 한다.', () => {
-    render(<BlogLink sessionId={mockData.sessionId} />);
+    renderBlogLink();
   });
 
   it('올바른 URL을 표시해야 한다.', () => {
-    const { getByText } = render(<BlogLink sessionId={mockData.sessionId} />);
-    expect(
-      getByText(`https://vanillog/posts/${mockData.sessionId}`),
-    ).toBeInTheDocument();
+    expect(getByText(blogLinkUrl)).toBeInTheDocument();
   });
 
   it('복사하기 버튼 클릭 시 내 블로그 링크가 복사되어야 한다.', async () => {
-    const { getByText, queryByText } = render(
-      <BlogLink sessionId={mockData.sessionId} />,
-    );
     const copyButton = getByText('복사하기');
+    expect(queryByText(successMessage)).toBeNull();
 
-    expect(queryByText('내 블로그 링크 복사 완료!')).toBeNull();
+    global.navigator.clipboard = {
+      writeText: jest.fn(() => Promise.resolve()),
+    };
 
-    await act(async () => {
-      fireEvent.click(copyButton);
+    fireEvent.click(copyButton);
+
+    act(() => {
+      jest.advanceTimersByTime(2000);
     });
 
-    await waitFor(() =>
-      expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
-        `https://vanillog/posts/${mockData.sessionId}`,
-      ),
-    );
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(blogLinkUrl);
 
-    await waitFor(() =>
-      expect(getByText('내 블로그 링크 복사 완료!')).toBeInTheDocument(),
-    );
+    const message = await findByText(successMessage);
+    expect(message).toBeInTheDocument();
 
-    await act(async () => {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+    act(() => {
+      jest.advanceTimersByTime(2000);
     });
-
-    expect(queryByText('내 블로그 링크 복사 완료!')).toBeNull();
+    expect(queryByText(successMessage)).toBeNull();
   });
 });
