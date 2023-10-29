82f424816bcd46143933a1fa00137c83531b70ef
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:54:31 +0900

    Refactor: LogOutButton 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/LogoutButton.test.js
+++ __tests__/Components/LogoutButton.test.js
@@ -2,22 +2,20 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { LogoutButton } from '@src/components/LogoutButton';
 import { signOut } from 'next-auth/react';
 
+const renderLogoutButtonAndGetElement = () => {
+  render(<LogoutButton />);
+  return screen.getByText(/Logout/i);
+};
+
 describe('<LogoutButton />', () => {
   it('로그아웃 버튼 컴포넌트가 올바르게 렌더링되어야 한다.', () => {
-    render(<LogoutButton />);
-
-    const buttonElement = screen.getByText(/Logout/i);
-
+    const buttonElement = renderLogoutButtonAndGetElement();
     expect(buttonElement).toBeInTheDocument();
   });
 
   it('로그아웃 버튼 컴포넌트 클릭 시, signOut 함수가 올바른 파라미터로 호출되어야 한다.', () => {
-    render(<LogoutButton />);
-
-    const buttonElement = screen.getByText(/Logout/i);
-
+    const buttonElement = renderLogoutButtonAndGetElement();
     fireEvent.click(buttonElement);
-
     expect(signOut).toHaveBeenCalled();
   });
 });
