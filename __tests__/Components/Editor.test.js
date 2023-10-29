d4f91bd34e18ef1026b5674764cd70cc1d2b3242
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:54:07 +0900

    Refactor: Editor 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/Editor.test.js
+++ __tests__/Components/Editor.test.js
@@ -1,51 +1,56 @@
 import React from 'react';
 import { render, screen, act, fireEvent } from '@testing-library/react';
 import Editor from '@src/components/Editor';
-import { useRouter } from 'next/navigation';
-import axios from 'axios';
+import { useRouter } from 'next/router';
 import EditorJS from '@editorjs/editorjs';
+import axios from 'axios';
+
+const setupEditorComponent = async (overrides = {}) => {
+  const props = {
+    author: 'testAuthorId',
+    postId: 'testPostId',
+    title: 'testTitle',
+    content: {},
+    error: null,
+    setError: jest.fn(),
+    isModify: false,
+    ...overrides,
+  };
+
+  await act(async () => {
+    render(<Editor {...props} />);
+  });
+
+  return props;
+};
+
+const clickSaveButton = async (screen) => {
+  const saveButton = screen.getByText('저장하기');
+  await act(async () => {
+    fireEvent.click(saveButton);
+  });
+};
+
+const mockEditorJSInstance = {
+  isReady: Promise.resolve(),
+  render: jest.fn(),
+  save: jest.fn().mockResolvedValue({
+    blocks: [{ type: 'paragraph', data: { text: 'testDataContent' } }],
+  }),
+};
+
+EditorJS.mockImplementation(() => mockEditorJSInstance);
 
 describe('<Editor />', () => {
   it('Editor 컴포넌트 렌더링 후 포스트가 올바르게 저장되어야 한다.', async () => {
-    useRouter.mockReturnValue({
-      push: jest.fn(),
-    });
+    const pushMock = jest.fn();
+    useRouter.mockReturnValue({ push: pushMock });
 
-    axios.post.mockResolvedValue({
-      data: {
-        status: 'success',
-      },
-    });
+    axios.post.mockResolvedValue({ data: { status: 'success' } });
 
-    const mockEditorJSInstance = {
-      isReady: Promise.resolve(),
-      render: jest.fn(),
-      save: jest.fn().mockResolvedValue({
-        blocks: [{ type: 'paragraph', data: { text: 'testDataContent' } }],
-      }),
-    };
+    await setupEditorComponent();
 
-    EditorJS.mockImplementation(() => mockEditorJSInstance);
-
-    await act(async () => {
-      render(
-        <Editor
-          author='testAuthorId'
-          postId='testPostId'
-          title='testTitle'
-          content={{}}
-          error={null}
-          setError={jest.fn()}
-          isModify={false}
-        />,
-      );
-    });
-
-    const saveButton = screen.getByText('저장하기');
-
-    await act(async () => {
-      fireEvent.click(saveButton);
-    });
+    await clickSaveButton(screen);
 
     expect(mockEditorJSInstance.save).toHaveBeenCalled();
     expect(axios.post).toHaveBeenCalled();
