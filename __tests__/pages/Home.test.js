b289d2dda1c7bc99044cd6947da7640c255d6c2f
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:58:28 +0900

    Refactor: Home Page 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/pages/Home.test.js
+++ __tests__/pages/Home.test.js
@@ -9,15 +9,15 @@ jest.mock('@src/components/Posts/PostList', () => {
   };
 });
 
-describe('<Home />', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
+const renderHomePage = async () => {
+  await act(async () => {
+    render(<Home />);
   });
+};
 
+describe('<Home />', () => {
   it('포스트 리스트 컴포넌트가 올바르게 렌더링 되어야 한다.', async () => {
-    await act(async () => {
-      render(<Home />);
-    });
+    await renderHomePage();
 
     expect(screen.getByText('Mocked Post List')).toBeInTheDocument();
   });
