9bc4c700267df484a5fde3ef14ae821287a8ea14
hamsterster <xhrrl003@gmail.com>
2023-10-29 14:57:23 +0900

    Refactor: BurgerMenu 유닛 테스트 코드 최적화 및 중복 제거

--- __tests__/Components/SideNavbar/BurgerMenu.test.js
+++ __tests__/Components/SideNavbar/BurgerMenu.test.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BurgerMenu } from '@src/components/SideNavbar/BurgerMenu';
 
+const renderBurgerMenu = (isOpen) => {
+  return render(<BurgerMenu isOpen={isOpen} />);
+};
+
 describe('<BurgerMenu />', () => {
   it('isOpen이 true인 경우 "open" 클래스로 렌더링 되어야 한다.', () => {
-    const { container } = render(<BurgerMenu isOpen={true} />);
+    const { container } = renderBurgerMenu(true);
     const divElement = container.firstChild;
+
     expect(divElement).toHaveClass('open');
   });
 
   it('isOpen이 false인 경우 "open" 클래스 없이 렌더링 되어야 한다.', () => {
-    const { container } = render(<BurgerMenu isOpen={false} />);
+    const { container } = renderBurgerMenu(false);
     const divElement = container.firstChild;
+
     expect(divElement).not.toHaveClass('open');
   });
 });
